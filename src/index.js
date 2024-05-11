275d63ff922896e20bbca4a2139f8b20d68d8273
Anamika <anamikaparoo@gmail.com>
2024-05-12 00:25:23 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import Sum from './Component/Sum';
 import BMI from './Component/BMI';
 import Timer from './Component/Timer';
+import Todo from './Component/Todo';
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer/>)
+root.render(<Todo/>)
